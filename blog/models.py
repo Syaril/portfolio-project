cb9c79b8742b4f53dc2d92ee87a016d0c22c4874
syarilezzuddeen <dr.syaril@gmail.com>
2018-08-17 11:50:25 +0800

    Add blog model and homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField(default='This is blog post.')
+    image = models.ImageField()
+    is_important = models.BooleanField(default=False)
