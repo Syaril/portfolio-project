fcceeecb7329491332308b4546670e70e8b407d0
syarilezzuddeen <dr.syaril@gmail.com>
2018-08-17 15:58:32 +0800

    added allblogs and detailed blog pages

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,11 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField(default='This is blog post.')
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     is_important = models.BooleanField(default=False)
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def allblogs(request):
+    getblogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'getblogs': getblogs})
+
+def detail(request, blog_id):
+    detailed_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detailed.html', {'blog': detailed_blog})
